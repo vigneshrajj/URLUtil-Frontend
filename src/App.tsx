574c04cfc1026118af578f58eab328040b4fa970
Vignesh Raj <vigneshhrajj@gmail.com>
2021-12-02 21:18:35 +0530

    frontend layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.scss';
+import { Switch } from '@mui/material';
+
+const label = { inputProps: { 'aria-label': 'PDF Switch' } };
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <div className='form'>
+                <h2>URL Util</h2>
+                <input type='text' placeholder='URL' />
+                <div className='switch-container'>
+                    <p>Image</p>
+                    <Switch {...label} defaultChecked />
+                    <p>PDF</p>
+                </div>
+                <button>Convert</button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
